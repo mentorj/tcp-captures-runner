63ebdef9faa135510f19e7f3f44180e9273a2ab0
deadbrain <jerome@javaxpert.com>
2020-04-23 23:49:15 +0200

    adds before method invokation.... general structure is ok now... needs a huge refactoring

--- src/main/java/com/javaxpert/demos/poc/network/captures/CapturesRunner.java
+++ src/main/java/com/javaxpert/demos/poc/network/captures/CapturesRunner.java
@@ -1,15 +1,31 @@
 package com.javaxpert.demos.poc.network.captures;
 
-import java.lang.annotation.Annotation;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.Optional;
 
 public class CapturesRunner {
     public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        Logger logger =LoggerFactory.getLogger(CapturesRunner.class);
         Class c =Class.forName(args[0]);
         Object suite =  c.newInstance();
         Method[] methods =c.getDeclaredMethods();
+        Config defaultConfig = ConfigFactory.parseResources("defaults.conf");
+        defaultConfig.resolve();
+        logger.debug(defaultConfig.root().render());
 
+        // find the BeforeCapture annotated method
+        //  TODO refactor & improve
+        Method beforeMethod = Arrays.stream(methods)
+                .filter(m-> Arrays.stream(m.getDeclaredAnnotations())
+                        .anyMatch(a -> a.annotationType().equals(BeforeCapture.class)))
+                .findFirst().get();
+        // find the captures & invoke them
         Arrays.stream(methods)
                 .filter(m ->
                         Arrays.stream(m.getDeclaredAnnotations())
@@ -17,6 +33,8 @@ public class CapturesRunner {
 
                 .forEach(m -> {
                     try {
+                        System.out.println("invoking before method");
+                        beforeMethod.invoke(suite,null);
                         System.out.println("before invoking capture");
                         m.invoke(suite, null);
                     } catch (Throwable t){
--- src/main/java/com/javaxpert/demos/poc/network/captures/CapturesSuite.java
+++ src/main/java/com/javaxpert/demos/poc/network/captures/CapturesSuite.java
@@ -1,12 +1,23 @@
 package com.javaxpert.demos.poc.network.captures;
 
+import org.pcap4j.core.PcapHandle;
+import org.pcap4j.core.PcapNetworkInterface;
+
 public class CapturesSuite {
+    private PcapNetworkInterface defaultInterface;
+    private PcapHandle handle;
     public CapturesSuite(){
         System.out.println("building object");
     }
     @NetworkCapture(captureName = "test.pcap")
     public void basicCapture(){
         System.out.print("basic capture launched");
+    }
+
+    @BeforeCapture
+    public void setupCapture(){
+        System.out.println("this is the before capture method");
+
     }
 
     public void useless(){
